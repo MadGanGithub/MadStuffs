901eadb3bcc3bca28e47417d51fd85ae72d54a8d
Madhav Ganesan <madhavganesan95@gmail.com>
2023-05-26 23:47:21 +0530

    hvj

--- server/server.js
+++ server/server.js
@@ -1,40 +1,7 @@
 import connectionDB from "./config/database.js";
 import dotenv from "dotenv";
+import app from './app.js'
 
-import express from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import userRoutes from "./routes/userRoutes.js";
-import cors from "cors";
-
-//import adminRoutes from './routes/adminRoutes.js'
-const app=express();
-
-//cors 
-app.use(cors({
-    credentials:true,
-    origin:"http://localhost:3000"
-}))
-
-//This converts request body to json 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
-
-//cookie parser(if not used,cookies will be undefined)
-app.use(cookieParser())
-
-//body parser(To view in postman)
-//app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json())
-
-
-
-
-//User
-// app.use("/",userRoutes)
-app.get("/",(req,res)=>{
-    res.json({"message":"hjyfhjfcyn"})
-})
 
 //Settings
 dotenv.config({ path: "./config/config.env" });
